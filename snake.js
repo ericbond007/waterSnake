af7ef2539278d2c5ef7c1ef739407b15de33bbbc
Eric Bond <ericbond007@gmail.com>
2018-10-03 18:11:34 -0400

    reducing tweet frequency.

--- snake.js
+++ snake.js
@@ -1,6 +1,6 @@
 var getWater = require('./getWater')
 var schedule = require('node-schedule');
 
-var stream = schedule.scheduleJob('0 05-23 * * *', function() {
+var stream = schedule.scheduleJob('0 05,12,19 * * *', function() {
   getWater(false);
 });
